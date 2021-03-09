d548a6e8f756de002373fbe7041f1492bb15ca42
nanjiangshu <nanjiang.shu@gmail.com>
2021-03-09 13:55:51 +0100

    bugfix: output the html file to outpath

--- frag1d/frag1d.sh
+++ frag1d/frag1d.sh
@@ -294,7 +294,7 @@ RunFrag1D(){ #$outpath $pssmFileListFile $frag1dParaFile #{{{
 
     # ==== Step 6  =============
     # generate output file in HTML format
-    exec_cmd "$BINPATH/txt2html.py $outpath/$outname.predfrag1d -o $outname.predfrag1d.html 1> /dev/null 2> $errFile"
+    exec_cmd "$BINPATH/txt2html.py $outpath/$outname.predfrag1d -o $outpath/$outname.predfrag1d.html 1> /dev/null 2> $errFile"
     CheckErrMsg $errFile
     echo "  $outpath/$outname.predfrag1d.html"
 }
